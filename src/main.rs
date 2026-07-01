b9e6fc507b966208fec99ea221d94bb7d9a2b12e
binilbp <biniloffcl@gmail.com>
2026-07-02 00:51:02 +0530

    Add subcommand; Add lib functions; Better Error handling

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,13 @@
-use clap::Parser;
+use clap::{Parser, Subcommand};
+use craw::{is_setup_done, run_setup};
+use std::error::Error;
 
 #[derive(Parser)]
+#[command(version, about)]
 struct Args {
+    #[command(subcommand)]
+    command: Option<Commands>,
+
     /// Pass content to LLM
     content: Option<String>,
 
@@ -9,21 +15,30 @@ struct Args {
     #[arg(short, long)]
     prompt: Option<String>,
 
-    /// Pass file content to LLM
+    /// Pass file to LLM
     #[arg(short, long)]
     file: Option<String>,
 }
 
-fn main() {
-    let args = Args::parse();
-
-    match args.content {
-        Some(content) => println!("{}", content),
-        None => empty_call(),
-    }
+#[derive(Subcommand)]
+enum Commands {
+    /// Start here. Setup CRAW
+    Setup,
 }
 
-fn empty_call() {
+fn main() -> Result<(), Box<dyn Error>> {
+    let args = Args::parse();
+    is_setup_done()?; //TODO
+
+    match args.command {
+        Some(Commands::Setup) => run_setup(),
+        None => print_about(),
+    }
+    Ok(())
+}
+
+//detailed custom about message when call CRAW without args
+fn print_about() {
     println!(
         "\
 \n------
@@ -34,8 +49,9 @@ CRAW 
 This tool was created with intention to learn RUST while trying to
 solve my annoyance of opening a browser or learning a new already
 available better tool to access LLM from a terminal.
-Use nerdfont for better experience.
-\n`craw --help` for more usage info
+Use nerdfont for better experience !
+\n-> `craw setup` to get started
+-> `craw help` for more usage info
 \nFeel free to suggest changes or express your issues
 ps: the naming was hard, Cli Rust Ai Wrapper ;)
 "
