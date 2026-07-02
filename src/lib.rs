cf530cf593b0f453c6f57a950a0998436cd67e6e
binilbp <biniloffcl@gmail.com>
2026-07-02 22:47:17 +0530

    Modify run_setup fn to also setup the api_service type in config

--- src/lib.rs
+++ src/lib.rs
@@ -1,25 +1,21 @@
+pub mod config;
 mod keyring;
 
 use std::{error::Error, io};
 
-pub fn run_setup() -> Result<(), Box<dyn Error>> {
+pub fn run_setup(cfg: &mut config::Config) -> Result<(), Box<dyn Error>> {
     let mut api_key = String::new();
 
     println!("CRAW Setup \n\nCRAW works using LLM API services (current support for GROQ only)");
     println!("Enter your GROQ api key: ");
-
     io::stdin().read_line(&mut api_key)?;
+
+    //add api to OS keyring
     keyring::add_key("groq_api_key", api_key.trim())?;
+    //also add the service type in config file
+    cfg.set_api_service(config::Services::GROQ)?;
+
     println!("API key set!");
 
-    Ok(())
-}
-
-pub fn is_setup_done() -> Result<(), &'static str> {
-    //TODO: add real logic
-    let done_setup = true;
-    if !done_setup {
-        return Err("CRAW setup not done. Please complete setup first. Run `craw setup`");
-    }
     Ok(())
 }
