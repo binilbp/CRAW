4bad96031057912d4790d21a543b40bf5b31cf64
binilbp <biniloffcl@gmail.com>
2026-07-02 22:44:56 +0530

    Add confy crate and Setup config file management

---
+++ src/config.rs
@@ -0,0 +1,21 @@
+use confy::ConfyError;
+use serde::{Deserialize, Serialize};
+
+#[derive(Default, Serialize, Deserialize)]
+pub struct Config {
+    pub api_service: Option<Services>,
+}
+
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum Services {
+    GROQ,
+}
+
+impl Config {
+    pub fn set_api_service(&mut self, service: Services) -> Result<(), ConfyError> {
+        self.api_service = Some(service);
+        confy::store("craw", "craw-config", self)?;
+        Ok(())
+    }
+}
