644c9708fc00f68fde996c865011c824dd511a8e
binilbp <biniloffcl@gmail.com>
2026-07-02 17:33:08 +0530

    Add delete_key fn; Add keyring test; Modify fn signatures for keyring functions

--- src/lib.rs
+++ src/lib.rs
@@ -9,23 +9,28 @@ pub fn run_setup() -> Result<(), Box<dyn Error>> {
     println!("Enter your GROQ api key: ");
 
     io::stdin().read_line(&mut api_key)?;
-    add_key(api_key.trim())?;
+    add_key("groq_api_key", api_key.trim())?;
     println!("API key set!");
 
     Ok(())
 }
 
-fn add_key(api_key: &str) -> KeyringResult<()> {
-    let entry = Entry::new("craw", "groq_api_key")?;
+fn add_key(service: &str, api_key: &str) -> KeyringResult<()> {
+    let entry = Entry::new("craw", service)?;
     entry.set_password(api_key)?;
-    // entry.delete_credential()?;
     Ok(())
 }
 
-fn get_key() -> KeyringResult<String> {
-    let entry = Entry::new("craw", "groq_api_key")?;
+fn get_key(service: &str) -> KeyringResult<String> {
+    let entry = Entry::new("craw", service)?;
     let key = entry.get_password()?;
     Ok(key)
+}
+
+fn delete_key(service: &str) -> KeyringResult<()> {
+    let entry = Entry::new("craw", service)?;
+    entry.delete_credential()?;
+    Ok(())
 }
 
 pub fn is_setup_done() -> Result<(), &'static str> {
@@ -35,4 +40,20 @@ pub fn is_setup_done() -> Result<(), &'static str> {
         return Err("CRAW setup not done. Please complete setup first. Run `craw setup`");
     }
     Ok(())
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    #[test]
+    fn keyring_test() {
+        let sample_key = String::from("my-sample-key");
+        add_key("sample-service", &sample_key).expect("failed to add key");
+        assert_eq!(
+            get_key("sample-service").expect("failed to retrieve key"),
+            sample_key
+        );
+        delete_key("sample-service").expect("failed to delete key");
+    }
 }
